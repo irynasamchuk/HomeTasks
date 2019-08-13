b36c637d0a26f2d4bccc12531c206181822d5492
Iryna Samchuk <samchukicat@gmail.com>
2019-08-13 08:52:00 +0300

    change ZooShop file

--- src/zooShopHomeTasks/ZooShop.java
+++ src/zooShopHomeTasks/ZooShop.java
@@ -1,16 +1,22 @@
 package zooShopHomeTasks;
+
 import java.util.List;
 
 public class ZooShop {
-   public String shopName;
-   public List<Animal> animals;
+    public String shopName;
+    public List<Animal> animals;
 
 
-    public ZooShop(String shopName) {
+//    public ZooShop(String shopName) {
+//        this.shopName = shopName;
+//    }
+//
+//    public ZooShop(List<Animal> animals) {
+//        this.animals = animals;
+//    }
+
+    public ZooShop(String shopName, List<Animal> animals) {
         this.shopName = shopName;
-    }
-
-    public ZooShop(List<Animal> animals) {
         this.animals = animals;
     }
 
@@ -23,44 +29,38 @@ public class ZooShop {
     }
 
     public void getAnimalsNames() {
-        System.out.println("Всі тваринки, що живуть у нас: ");
-        for(Animal animal : animals) {
+        System.out.println("\nВсі тваринки, що живуть у нас: ");
+        for (Animal animal : animals) {
             System.out.println(animal.getName());
         }
-        System.out.println(" ");
-        }
+    }
 
-    public void getFourLegsAnimals(){
-        System.out.println("Тварини, що мають 4 ноги:");
-        for(Animal animal : animals){
-            if(animal.getLegsCount() == 4){
+    public void getFourLegsAnimals() {
+        System.out.println("\nТварини, що мають 4 ноги:");
+        for (Animal animal : animals) {
+            if (animal.getLegsCount() == 4) {
                 System.out.println(animal.getName());
             }
         }
-
-        System.out.println(" ");
     }
 
-    public void getCheapAnimals(){
-        System.out.println("Тварини, які коштують менше 100$:");
-        for(Animal animal : animals){
-            if(animal.getPrice() < 100){
-                System.out.println(animal.getName() + " " + animal.getPrice());
-            }
-
-        }
-
-        System.out.println(" ");
-    }
-
-    public void getExpensiveAnimals(){
-        System.out.println("Тварини, які коштують більше 100$:");
-        for(Animal animal : animals){
-            if(animal.getPrice() > 100){
+    public void getCheapAnimals() {
+        System.out.println("\nТварини, які коштують менше 100$:");
+        for (Animal animal : animals) {
+            if (animal.getPrice() < 100) {
                 System.out.println(animal.getName() + " " + animal.getPrice());
             }
         }
-        System.out.println(" ");
+    }
+
+    public void getExpensiveAnimals() {
+        System.out.println("\nТварини, які коштують більше 100$:");
+        for (Animal animal : animals) {
+            if (animal.getPrice() > 100) {
+//                System.out.println(animal.getName() + " " + animal.getPrice());
+                System.out.println(String.format("%s %s", animal.getName(), animal.getPrice()));
+            }
+        }
     }
 
 
